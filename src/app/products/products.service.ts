1155b3d4dbb6f038cf4007e228c19e4b48780e93
walteragile <wcarpiom@gmail.com>
2023-11-22 22:56:21 -0500

    10b RxJS operators: pipe, tap, catchError for exception handling

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './product';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +12,21 @@ export class ProductsService {
   constructor(private http: HttpClient) {}
 
   getProducts(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>(this.productUrl);
+    return this.http.get<IProduct[]>(this.productUrl).pipe(
+      tap(data => console.log('All', JSON.stringify(data))),
+      catchError(this.handleError),
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error ocurred: ${err.error.message}`;
+    } 
+    else {
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(() => errorMessage);
   }
 }
