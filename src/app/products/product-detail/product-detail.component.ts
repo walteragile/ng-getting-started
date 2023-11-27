99f93d39b0ef0f9cf704a075fef5da6222911604
walteragile <wcarpiom@gmail.com>
2023-11-26 21:25:25 -0500

    13.0 Bootstrap styles and columns for product detail page

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,36 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IProduct } from '../product';
+import { ProductsService } from '../products.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent implements OnInit {
+export class ProductDetailComponent implements OnInit, OnDestroy {
   pageTitle = 'Product Detail';
-  product: IProduct | undefined;
+  product!: IProduct;
+  subscription!: Subscription;
+  errorMessage = '';
 
   constructor(
+    private productService: ProductsService,
     private route: ActivatedRoute,
     private router: Router,
   ) { }
-
+  
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.pageTitle += `: ${id}`
-    this.product = {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2021",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "assets/images/leaf_rake.png"
-    };
+    this.subscription = this.productService.getProducts().subscribe({
+      next: products => {
+        this.product = products.filter(p => p.productId == id)[0];
+        if (this.product) {
+          this.pageTitle += `: ${this.product.productName}`;
+        }
+        else {
+          this.onBack();
+        }
+      },
+      error: err => this.errorMessage = err,
+    });
   }
 
   onBack(): void {
     this.router.navigate(['/products']);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
